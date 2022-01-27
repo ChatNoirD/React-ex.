6ea6c42a062979dff61403f64d6a5d1a8c0f8fdf
ChatNoir <igor.dubrovin.od@gmail.com>
2022-01-27 18:15:08 +0200

    Third task

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-// Ex 2: Need to reverse a string called in function
+// Ex 3: Need find a factorial of num
 
-function reverseString(str) {
+function factorialize(num) {
 
-  str = str.split('').reverse().join('');
+  (num != 1) ? num * factorial(num - 1) : 1;
 
-  return str;
+  return num;
 }
 
-function reverseString(str) {
-
-  let arr = str.split('');
-  let newArr = [];
-
-  for (let i = 0; i <= arr.length; i++) {
-    while (newArr.length <= arr.length) {
-        let temp = arr.pop();
-        newArr.push(temp);
-    } break;
-}
-  str = newArr.join('');
-
-  return str;
-}
-
-// We can solve this task two ways: first is shorter - make an array from the string, use 'reverse' method for the array and make it a string again; second way is to create
-// array from a string again, make new array and then with 'for' and 'while' cicles we must take items from first array and push it to the second one. Then make a string from the second array.
+console.log(factorialize(5));
