923779aa797d5199cbca17d264569673697fd507
ChatNoir <igor.dubrovin.od@gmail.com>
2022-01-27 14:49:01 +0200

    Ex 1 solved

---
+++ celcius_to_fahrenheit.js
@@ -0,0 +1,12 @@
+// Ex 1: Need to create function that converts Celcius to Fahrenheit
+
+function convertToF(celcius) {
+    let fahrenheit = null;
+
+    fahrenheit = celcius * 9 / 5 + 32;
+
+    return fahrenheit;
+  }
+  
+convertToF(0);
+// To solve this task we just create a function with mathematical expression including variable "celcius". No need to solve it another way
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Ex 1: Need to create function that converts Celcius to Fahrenheit
+// Ex 2: Need to reverse a string called in function
+function reverseString(str) {
+
+    str = str.split('').reverse().join('');
+
+    return str;
+  }
+  
+  console.log(reverseString("hello"));
+  // To solve this task we create an array from this string by split method, reverse it and create a string again by join method
